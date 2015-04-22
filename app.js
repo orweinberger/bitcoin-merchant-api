1f9bf959c214c0ad7e8140742a3ef97263bc2500
orweinberger <orweinberger@gmail.com>
2015-04-22 18:22:33 +0300

    #4 minor tweaks

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -59,8 +57,8 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(config.general.port, config.general.bind, function (err, result) {
-
+app.listen(config.general.port, config.general.bind, function () {
+  console.log('Bitcoin web point of sale listening on port ' + config.general.port);
 });
 
 module.exports = app;
